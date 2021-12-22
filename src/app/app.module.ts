2ae270f2aa868892854ee29191e74ce44fa5b637
thiru <mastiffthiru@gmail.com>
2021-12-22 19:36:27 +0530

    added additional buttons and daterange selector

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {TimePeriodModule} from "./time-period/time-period.module";
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-    imports: [
-        BrowserModule,
-        TimePeriodModule
-    ],
+  imports: [
+    BrowserModule,
+    TimePeriodModule,
+    NoopAnimationsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/time-period/time-period.module.ts
+++ src/app/time-period/time-period.module.ts
@@ -1,20 +1,25 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { DataAvailabilityViewerComponent } from './data-availability-viewer/data-availability-viewer.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {DataAvailabilityViewerComponent} from './data-availability-viewer/data-availability-viewer.component';
 import {HttpClientModule} from "@angular/common/http";
-
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatNativeDateModule} from "@angular/material/core";
 
 
 @NgModule({
-    declarations: [
-        DataAvailabilityViewerComponent
-    ],
-    exports: [
-        DataAvailabilityViewerComponent
-    ],
-    imports: [
-        CommonModule,
-      HttpClientModule
-    ]
+  declarations: [
+    DataAvailabilityViewerComponent
+  ],
+  exports: [
+    DataAvailabilityViewerComponent
+  ],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatDatepickerModule
+  ]
 })
 export class TimePeriodModule { }
